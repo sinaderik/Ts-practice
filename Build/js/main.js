906d1c8ce52701d214cd51290f53cb1eb85adc63
Sina <sinaderik7@gmail.com>
2024-09-06 22:42:25 +0330

    feat: function and array are added

--- Build/js/main.js
+++ Build/js/main.js
@@ -2,3 +2,25 @@
 let num1 = 2;
 let num2 = 6;
 console.log(num2 / num1);
+let userName = 'sina';
+let isLoading;
+let postId;
+let album;
+const sum = (a, b) => {
+    return a + b;
+};
+let stringArray = ['sina', 'amir', 'reza'];
+let carArray = ['bmw', 'benz', 1985];
+let mixedData = ['cup', true, 523];
+// its incorrect because stringArray is typed string
+// stringArray.push(12)
+stringArray.push('javad');
+// its correct because carArray is union type of string and number
+carArray[0] = 78;
+carArray.unshift(45);
+// its incorrect because stringArray is typed string
+// stringArray=carArray
+// its correct because carArray is union type of string and number
+carArray = stringArray;
+// its correct because carArray is union type of string , number , boolean
+// carArray=mixedData
--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,33 @@ let num1: number = 2
 let num2: number = 6
 console.log(num2 / num1)
 
+let userName: string = 'sina'
+let isLoading: boolean;
+let postId: number | string;
+let album: any
+
+const sum = (a: number, b: number): number => {
+    return a + b
+}
+
+let stringArray = ['sina', 'amir', 'reza']
+let carArray = ['bmw', 'benz', 1985]
+let mixedData = ['cup', true, 523]
+
+// its incorrect because stringArray is typed string
+// stringArray.push(12)
+stringArray.push('javad')
+
+// its correct because carArray is union type of string and number
+carArray[0]=78
+carArray.unshift(45)
+
+// its incorrect because stringArray is typed string
+// stringArray=carArray
+
+// its correct because carArray is union type of string and number
+carArray=stringArray
+
+// its correct because carArray is union type of string , number , boolean
+// carArray=mixedData
+
