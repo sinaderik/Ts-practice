16ab73f4d3468711910c9edbe562a39d74f27f92
Sina <sinaderik7@gmail.com>
2024-09-10 12:34:12 +0330

    feat: keyof and typeof added to the project

--- Build/js/indexSignature.js
+++ Build/js/indexSignature.js
@@ -1,4 +1,6 @@
 "use strict";
+// thats how we loop on an object that with the use of index signature 
+// if we didnt have the index signature we would have problem
 const myCar = {
     company: 'benz',
     name: 'c200',
@@ -8,4 +10,18 @@ const carName = 'name';
 console.log(myCar[carName]);
 for (const key in myCar) {
     console.log(`${key} : ${myCar[key]}`);
+}
+// but here is a new problem : we can access to property that doesnt exist in our object
+console.log(myCar.engine);
+const newStudent = {
+    name: 'reza',
+    city: 'tehran',
+    code: 205
+};
+for (const key in newStudent) {
+    console.log(`${key} : ${newStudent[key]}`);
+}
+// now lets say we dont have access to the interface and we dont know our object what type is it :
+for (const key in newStudent) {
+    console.log(`${key} : ${newStudent[key]}`);
 }
--- src/indexSignature.ts
+++ src/indexSignature.ts
@@ -1,3 +1,6 @@
+// thats how we loop on an object that with the use of index signature 
+// if we didnt have the index signature we would have problem
+
 interface Car {
     [index: string]: string | number
     company: string,
@@ -11,9 +14,33 @@ const myCar: Car = {
     name: 'c200',
     model: 2022,
 }
-const carName:string='name'
+const carName: string = 'name'
 console.log(myCar[carName])
 
 for (const key in myCar) {
     console.log(`${key} : ${myCar[key]}`)
+}
+// but here is a new problem : we can access to property that doesnt exist in our object
+console.log(myCar.engine)
+
+// thats where keyof , typeof comes in handy : with the use of keyof we can loop over an object without 
+// need of index signature 
+interface Student {
+    name: string,
+    city: string,
+    code: number,
+}
+const newStudent: Student = {
+    name: 'reza',
+    city: 'tehran',
+    code: 205
+}
+for(const key in newStudent){
+    console.log(`${key} : ${newStudent[key as keyof Student]}`)
+}
+
+// now lets say we dont have access to the interface and we dont know our object what type is it :
+
+for(const key in newStudent){
+    console.log(`${key} : ${newStudent[key as keyof typeof newStudent]}`)
 }
