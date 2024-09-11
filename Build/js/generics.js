6b940f97cbdf416e588601dce17ca6e2906f5f80
Sina <sinaderik7@gmail.com>
2024-09-11 16:55:51 +0330

    feat: isTure and checkTrueOrFalse are created to check the the truthy and falsy values

--- Build/js/generics.js
+++ Build/js/generics.js
@@ -11,3 +11,21 @@ const isObj = (arg) => {
     }
     return false;
 };
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (isObj(arg) && Object.keys(arg).length === 0) {
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg };
+};
+const checkTrueOrFalse = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (isObj(arg) && Object.keys(arg).length === 0) {
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg };
+};
--- src/generics.ts
+++ src/generics.ts
@@ -7,9 +7,35 @@ const echo = <T>(arg: T): T => {
 }
 
 const isObj = <T>(arg: T): boolean => {
-    if (typeof arg === 'object' && !Array.isArray(arg) && arg !== null){
+    if (typeof arg === 'object' && !Array.isArray(arg) && arg !== null) {
         return true
     }
     return false
+}
+
+const isTrue = <T>(arg: T): { arg: T, is: boolean } => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false }
+    }
+    if (isObj(arg) && Object.keys(arg as keyof T).length === 0) {
+        return { arg, is: false }
+    }
+    return { arg, is: !!arg }
+}
+
+// same function but using interface for its return type
+interface CheckBoolean<T> {
+    arg: T,
+    is: boolean
+}
+
+const checkTrueOrFalse = <T>(arg: T): CheckBoolean<T> => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false }
+    }
+    if (isObj(arg) && Object.keys(arg as keyof T).length === 0) {
+        return { arg, is: false }
+    }
+    return { arg, is: !!arg }
 }
 
