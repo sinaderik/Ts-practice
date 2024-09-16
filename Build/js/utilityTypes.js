a285b4b8c74878b5093fb6d4eebb2ff5f720467e
Sina <sinaderik7@gmail.com>
2024-09-16 18:28:00 +0330

    record utility type is added to project

--- Build/js/utilityTypes.js
+++ Build/js/utilityTypes.js
@@ -1,4 +1,5 @@
 "use strict";
+// Partial
 const updateAssignment = (assign, propsToUpdate) => {
     return Object.assign(Object.assign({}, assign), propsToUpdate);
 };
@@ -9,3 +10,22 @@ const student1 = {
 };
 const resultAssignment = updateAssignment(student1, { grade: 18 });
 console.log(resultAssignment);
+// required and readonly
+const recordAssignment = (assign) => {
+    // some logic
+    return assign;
+};
+// console.log(recordAssignment(student1))
+const assignVerified = student1;
+// assignVerified.grade=54
+// Record
+// here we're saying that all values and keys must be string
+const colorCode = {
+    blue: '#8520',
+    green: '#2368',
+    red: '99025'
+};
+const finallGrade = {
+    kelly: 'A',
+    Pitter: 'C'
+};
--- src/utilityTypes.ts
+++ src/utilityTypes.ts
@@ -4,7 +4,7 @@ interface Assignment {
     grade: number,
     verified?: boolean
 }
-
+// Partial
 const updateAssignment = (assign: Assignment, propsToUpdate: Partial<Assignment>): Assignment => {
     return { ...assign, ...propsToUpdate }
 }
@@ -16,3 +16,30 @@ const student1: Assignment = {
 
 const resultAssignment: Assignment = updateAssignment(student1, { grade: 18 })
 console.log(resultAssignment)
+
+// required and readonly
+
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+    // some logic
+    return assign
+}
+// console.log(recordAssignment(student1))
+const assignVerified:Readonly<Assignment>=student1;
+// assignVerified.grade=54
+
+
+// Record
+// here we're saying that all values and keys must be string
+const colorCode:Record<string,string>={
+    blue:'#8520',
+    green:'#2368',
+    red:'99025'
+}
+
+type StudentName='kelly' | 'Pitter' 
+type LetterGrades='A'|'B'|'C'|'D'
+
+const finallGrade:Record<StudentName,LetterGrades>={
+    kelly:'A',
+    Pitter:'C'
+}
